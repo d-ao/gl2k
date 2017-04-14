59e6b327cf8cdfb57ee1d1e1c4b4d80c2ffa4ba7
Joachim Grubelnik <j.grubelnik@openat.at>
2017-04-14 08:24:48 +0200

    [ADD] remove html code for local Iframe tests

--- addons/gl2k_config/static/src/html/Neuer Ordner/Spenden_Global2000_files/jquery.mobile.config.js
+++
@@ -1,9 +0,0 @@
-// needs to be run *before* jquery.mobile is loaded
-jQuery(document).bind('mobileinit',function(){
-  jQuery.mobile.page.prototype.options.keepNative = "input[type=submit], button";
-  jQuery.mobile.page.prototype.options.theme = "o";
-  jQuery.mobile.ajaxEnabled = false;
-  jQuery.mobile.pushStateEnabled = false;
-  jQuery.mobile.selectmenu.prototype.options.nativeMenu = false;
-  jQuery.mobile.selectmenu.prototype.options.loadingMessage = false;
-});
