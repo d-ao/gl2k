fec0677c4ee2d9cdd0133e5a90367e092bf3ca70
Michael Karrer <michaelkarrer81@gmail.com>
2020-10-28 17:19:16 +0100

    [UPDATE] o8r389 [ADD] Three new product themes for evelyn

--- addons/gl2k_product_themes/models/product_template.py
+++ addons/gl2k_product_themes/models/product_template.py
@@ -9,4 +9,7 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     website_theme = fields.Selection(selection_add=[('gl2k_inline', 'Global 2000 Inline'),
-                                                    ('gl2k_inline_test', 'Global 2000 Inline TEST')])
+                                                    ('gl2k_inline_test', 'Global 2000 Inline Testtheme'),
+                                                    ('gl2k_theme_a', 'Global 2000 Theme A'),
+                                                    ('gl2k_theme_b', 'Global 2000 Theme B'),
+                                                    ('gl2k_theme_c', 'Global 2000 Theme C')])
